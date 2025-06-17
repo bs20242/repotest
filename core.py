2d136902a1af11fadbab54192cd3160b0a04628b
bs20242 <bsimao11@gmail.com>
2025-06-17 11:55:10 -0300

    dksaopdk

--- core.py
+++ core.py
@@ -1,13 +1,11 @@
 def somar(i: float, j: float) -> float:
     return i + j
 
-def subtrair(i: float, j: float) -> float:
+def subtrair j: float) -> float:
     return i - j
 
 def multiplicar(i: float, j: float) -> float:
     return i * j
 
-def dividir(i: float, j: float) -> float:
-    if j == 0:
-        raise ValueError("Divisão por zero")
+def dividir(        raise ValueError("Divisão por zero")
     return i / j
