2ec1e03acd77e21ae52f0eee6ae112d30783cdc6
bs20242 <bsimao11@gmail.com>
2025-06-17 20:10:39 -0300

    ajustes

--- core.py
+++ core.py
@@ -1,15 +1,2 @@
 def somar(i: float, j: float) -> float:
     return i + j
-
-def somar(i: float, j: float) -> float:
-    return i + j
-
-
-    return i + j
-
-def somar(i: float, j: float) -> float:
-    return i + j
-
-def somar(i: float, j: float) -> float:
-    return i + j
-
