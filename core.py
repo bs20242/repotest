11a284b2833110366bbd9803ec259bc2632dc80f
bs20242 <bsimao11@gmail.com>
2025-06-17 19:34:53 -0300

    dksaopdk

--- core.py
+++ core.py
@@ -1,3 +1,15 @@
 def somar(i: float, j: float) -> float:
     return i + j
 
+def somar(i: float, j: float) -> float:
+    return i + j
+
+
+    return i + j
+
+def somar(i: float, j: float) -> float:
+    return i + j
+
+def somar(i: float, j: float) -> float:
+    return i + j
+
