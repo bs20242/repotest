7c9c94728827c59ede441986004ee076e68de064
bs20242 <bsimao11@gmail.com>
2025-06-17 22:09:55 -0300

    teste

--- core.py
+++ core.py
@@ -1,3 +1,15 @@
-def somar(i: float, j: float) -> float:
-	return i + j
+from c_lib import somar, subtrair, multiplicar, dividir
 
+print("Testando a Calculadora C_lib:\n")
+
+# Soma
+print("Soma 5 + 3 =", somar(5, 5))  
+
+# Subtração
+print("Subtração 10 - 4 =", subtrair(16, 4))  
+
+# Multiplicação
+print("Multiplicação 7 * 6 =", multiplicar(7, 3))  
+
+# Divisão
+print("Divisão 20 / 5 =", dividir(20, 5)) 
