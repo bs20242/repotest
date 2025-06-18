f078fceebd02753b477b241ef7341b8606f85e2d
bs20242 <bsimao11@gmail.com>
2025-06-18 19:45:21 -0300

    aaa

--- duo2.py
+++ duo2.py
@@ -3,12 +3,6 @@ from c_lib import somar, subtrair
 # Soma
 print("Soma 5 + 3 =", somar(5, 3))  
 
-# Subtração
-print("Subtração 10 - 4 =", subtrair(10, 4))
-
-print(subtrair(15, 4))
-
-print(subtrair(15, 4))
 
 
 
