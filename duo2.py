a627b4eeeeac1aa8e148a10eb63a5f452a78b881
bs20242 <bsimao11@gmail.com>
2025-06-17 23:16:57 -0300

    3

--- duo2.py
+++ duo2.py
@@ -6,9 +6,5 @@ print("Soma 5 + 3 =", somar(5, 3))
 # Subtração
 print("Subtração 10 - 4 =", subtrair(10, 4))  
 
-# Subtração
-print("Subtração 10 - 4 =", subtrair(10, 4))  
 
-# Multiplicação
-print("Multiplicação 7 * 6 =", multiplicar(7, 6))
 
