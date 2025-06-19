1fe70613cec8be16e6bab7d79ffff483bc29bf7f
bs20242 <bsimao11@gmail.com>
2025-06-18 22:24:53 -0300

    6

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from fastapi import FastAPI
+from c_lib import somar, subtrair, multiplicar, dividir
 
+print("Testando a Calculadora C_lib:\n")
 
+# Soma
+print("Soma 5 + 3 =", somar(5, 3))  
+
+# Subtração
+print("Subtração 10 - 4 =", subtrair(10, 4))  
+
+# Multiplicação
+print("Multiplicação 7 * 6 =", multiplicar(7, 6))
+
+# Multiplicação
+print(multiplicar(7, 7))
