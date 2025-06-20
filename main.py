acb9f0be970f2c448ca7fdc6f120f743d9d2819c
bs <bsimao11@gmail.com>
2025-06-20 14:54:02 -0300

    mnt

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ print("Soma 5 + 3 =", somar(5, 3))
 print("Subtração 10 - 4 =", subtrair(10, 4))  
 
 # Multiplicação
-print("Multiplicação 7 * 6 =", multiplicar(7, 6))
+print("Multiplicação 8 * 9 =", multiplicar(8, 9))
 
